b5ffb7c6ae4cf6d88cb212430cf1f02d186a6be8
manuelatabacaru <manuela.tabacaru@gmail.com>
2021-08-06 20:38:19 +0300

    no message

--- utils/messages.js
+++ utils/messages.js
@@ -3,7 +3,6 @@
 export default class Messages {
    constructor () {
 
-   	this.classPresent = 'Class is missing when it should be present'
    	this.productAddedSuccessfully = "Message: 'Product successfully added to your shopping car' is missing when it should be present"
    	this.iconOk = 'Green check mark is not displayed in UI when it should be'
    	this.agreeTerms = "Checkbox'I agree to the terms of service and will adhere to them unconditionally. (Read the Terms of Service)' is missing from UI when it should be present"
